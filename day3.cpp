bca334a4f69cf7544ead3b6dbcde8721329427dd
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-06-20 11:11:55 +0530

    Add solution for minimum page allocation problem
    Implemented a solution for the minimum page allocation problem, allowing for the distribution of books among students while minimizing the maximum number of pages assigned to a student.

--- day3.cpp
+++ day3.cpp
@@ -50,3 +50,74 @@ public:
         
     }
 };
+
+
+//little bit hard but easy to understaand in this we have minimum page allocation problem form the geek for geeks so hgere the solution that i understadnd tried to codee
+
+class Solution {
+public:
+
+    bool isPossible(vector<int>& arr, int k, int mid) {
+
+        int student = 1;
+        int pages = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+
+            // Single book exceeds limit
+            if (arr[i] > mid) {
+                return false;
+            }
+
+            if (pages + arr[i] <= mid) {
+                pages += arr[i];
+            }
+            else {
+                student++;
+                pages = arr[i];
+
+                if (student > k) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    int findPages(vector<int> &arr, int k) {
+
+        int n = arr.size();
+
+        if (k > n) {
+            return -1;
+        }
+
+        int sum = 0;
+        int maxi = 0;
+
+        for (int x : arr) {
+            sum += x;
+            maxi = max(maxi, x);
+        }
+
+        int s = maxi;
+        int e = sum;
+        int ans = -1;
+
+        while (s <= e) {
+
+            int mid = s + (e - s) / 2;
+
+            if (isPossible(arr, k, mid)) {
+                ans = mid;
+                e = mid - 1;
+            }
+            else {
+                s = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
