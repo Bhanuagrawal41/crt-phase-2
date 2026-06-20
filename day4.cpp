b70479a808375edace4af95243d12abc53cf8f70
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-06-20 15:25:10 +0530

    Update day4.cpp

--- day4.cpp
+++ day4.cpp
@@ -119,3 +119,34 @@ public:
         return ans;
     }
 };
+
+
+
+// leet code - 367
+// valid perfect square using binary serach
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long start = 1;
+        long long end  = num;
+
+        while(start <= end){
+            long long mid = (start + end) / 2;
+
+            if((mid*mid) == num){
+                return true;
+            } else if((mid*mid) < num){
+                start = mid + 1;
+            } else{
+                end = mid-1;
+            }
+
+
+
+            
+        }
+        return false;
+        
+    }
+};
