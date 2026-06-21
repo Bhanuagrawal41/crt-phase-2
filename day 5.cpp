89dd407aa2cdfe21c9df7837924689742edb97ef
Bhanu Agrawal <bhanuagrawal25641@gmail.com>
2026-06-21 14:42:22 +0530

    Add recursive minmax function in Solution class
    Implemented a recursive function to find minimum and maximum values in an array.

--- day 5.cpp
+++ day 5.cpp
@@ -44,3 +44,59 @@ int main()
     int i = 1;
     print(i, 4);
 }
+// finding min and max using revursion 
+class Solution {
+  public:
+  
+  
+      void minmax(vector<int> &arr, int i, int min, int max, vector<int>&ans){
+          
+          if(i == arr.size() ){
+              ans.push_back(min);
+              ans.push_back(max);
+              
+              return;
+          }
+          
+          
+          if(arr[i] < min){
+              min = arr[i];
+          }
+          
+          
+          if(arr[i] > max){
+              max = arr[i];
+          }
+          
+          
+          
+          minmax(arr, i+1, min, max, ans);
+          
+          
+      }
+  
+     
+    vector<int> getMinMax(vector<int> &arr) {
+        // code here
+        // base case
+        
+        // int n = arr.size();
+        vector<int> ans;
+        
+        int max = INT_MIN;
+        
+        
+        int min = INT_MAX;
+        
+        minmax(arr, 0, min, max, ans);
+        return ans;
+        
+        
+       // logic 
+       
+       
+        
+        
+        
+    }
+};
