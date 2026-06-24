5ef10232af5c3a946424bc7211793b221b32dcd7
Bhanu Agrawal <bhanuagrawal25641@gmail.com>
2026-06-24 13:41:19 +0530

    Update day8.cpp

--- day8.cpp
+++ day8.cpp
@@ -1,1 +1,35 @@
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL){
+            return NULL;
+        }
+
+        ListNode *curr = head;
+        while(curr != NULL && curr->next != NULL){
+            if(curr -> val == curr->next ->val){
+                ListNode *backup = curr->next->next;
+                delete(curr->next);
+                curr -> next = backup;
+
+            }
+            else{
+                curr = curr -> next;
+            }
+        }
+
+        return head;
+    }
+    
+};
