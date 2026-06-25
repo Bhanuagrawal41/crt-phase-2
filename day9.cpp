4297aa5d39c0b7e61a1994f7c9f93275a1c40dcb
Bhanu Agrawal <bhanuagrawal25641@gmail.com>
2026-06-25 14:48:07 +0530

    Update day9.cpp

--- day9.cpp
+++ day9.cpp
@@ -48,3 +48,94 @@ int main()
    }
    return 0;
 }
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+//khud ka agar mugje stack banana hai then using class
+
+class Node{
+    public:
+    int value;
+    Node *next;
+    Node(int v){
+        value = v;
+        next = NULL;
+    }
+    
+};
+
+class Stack{
+    public:
+    Node *head;
+    
+  Stack()
+  {
+      head = NULL;
+  }
+  
+  
+  // insert at first becoz constant time complextiy because afete this we don't have touse
+  // we dont have to use the loop to excess the last elemtent
+  void push(int v){
+      if(head == NULL){
+      Node *n = new Node(v);
+      head = n;
+  }
+  else{
+      Node *n = new Node(v);
+      n->next = head;
+      head = n;
+  }
+  
+  
+  
+}
+
+// delete at first same logic as the previous one
+void pop(){
+    if(head == NULL){
+        return;
+    } else if(head -> next == NULL){
+        delete head;
+        head = NULL;
+    } else{
+        Node *backup = head -> next;
+        delete head;
+        head = backup;
+    }
+    
+   }
+   
+   int top(){
+       return head -> value;
+   }
+   
+   bool empty(){
+       if(head == NULL)
+           return true;
+           
+        else
+        return false;
+        
+       
+   }
+};
+
+
+int main() 
+{
+   Stack st;
+   st.push(1);
+   st.push(2);
+   st.push(3);
+
+
+   while(!st.empty())
+   {
+    cout << st.top()<< " ";
+    st.pop();
+   }
+   return 0;
+}
