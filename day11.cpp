43f08ac672d56dcd27088d00f2dfb7b66df22190
Bhanu Agrawal <bhanuagrawal25641@gmail.com>
2026-06-27 14:45:21 +0530

    Add sumBetweenTwoKth function to calculate sum between Kth elements
    Implement sumBetweenTwoKth function to calculate the sum of elements between the K1-th and K2-th smallest elements.

--- day11.cpp
+++ day11.cpp
@@ -13,3 +13,45 @@ class Solution {
         return max_heap.top();
     }
 };
+
+class Solution {
+  public:
+ 
+  long long kthsmallest(long long A[], long long N, long long k ){
+      priority_queue<long long> max_heap;
+      
+      for(int i = 0; i<N; i++){
+          max_heap.push(A[i]);
+          if(max_heap.size()>k)
+          max_heap.pop();
+      }
+      
+      return max_heap.top();
+  }
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+    long long sumBetweenTwoKth(long long A[], long long N, long long K1, long long K2) {
+        // Your code goes here
+        
+       long long first = kthsmallest(A,N,K1);
+       long long second = kthsmallest(A, N, K2);
+       
+       long long sum = 0;
+       for(int i = 0; i<N; i++){
+           if(A[i] > first && A[i] < second){
+               sum += A[i];
+           }
+       }
+       return sum;
+        
+        
+        
+    }
+};
