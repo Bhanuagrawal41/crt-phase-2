4b1132c9f422c5ea65de51b3667feccac0d669ae
Bhanu Agrawal <bhanuagrawal25641@gmail.com>
2026-07-01 15:49:58 +0530

    Implement memoization for climbStairs  using dynamic programming

--- day14.cpp
+++ day14.cpp
@@ -30,3 +30,24 @@ public:
         
     }
 };
+
+class Solution {
+public:
+
+    int t[46];
+   Solution() {
+        memset(t, -1, sizeof(t));
+    }
+
+    int climbStairs(int n) {
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        if(t[n] != -1){
+            return t[n];
+        }
+        return t[n] = climbStairs(n-1) + climbStairs(n-2);
+        
+        
+    }
+};
