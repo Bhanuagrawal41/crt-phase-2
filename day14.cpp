cf3198759230386286302ccaad9674d3860f1355
Bhanu Agrawal <bhanuagrawal25641@gmail.com>
2026-07-01 15:51:46 +0530

    Implement dynamic programming solution for coin change

--- day14.cpp
+++ day14.cpp
@@ -51,3 +51,36 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    int dp[301][5001];
+
+    int solve(vector<int>& coins, int amount, int i) {
+
+        if (amount == 0)
+            return 1;
+
+        if (i == coins.size())
+            return 0;
+
+        if (dp[i][amount] != -1)
+            return dp[i][amount];
+
+        int take = 0;
+
+        if (coins[i] <= amount)
+            take = solve(coins, amount - coins[i], i);
+
+        int skip = solve(coins, amount, i + 1);
+
+        return dp[i][amount] = take + skip;
+    }
+
+    int change(int amount, vector<int>& coins) {
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(coins, amount, 0);
+    }
+};
